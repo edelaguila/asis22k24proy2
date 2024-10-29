807b43ea26c1bee289e9e54cab295ec656a425d4
Ismar Leonel Cortez Sanchez <icortezs@miumg.edu.gt>
2024-10-28 23:41:57 -0600

    Modulo Encabezado y detalle planillas

---
+++ Codigo/Modulos/Nominas/Planilla_Nominas/Capa_Controlador_Planilla/Controlador.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Modelo_Planilla;
+
+namespace Capa_Controlador_Planilla
+{
+    public class Controlador
+    {
+        Sentencias sn = new Sentencias();
+
+        public bool EjecutarCalculoPlanilla(int pkRegistroPlanillaDetalle, int fkIdRegistroPlanillaEncabezado, int fkClaveEmpleado)
+        {
+            return sn.CalcularPlanillaDetalle(pkRegistroPlanillaDetalle, fkIdRegistroPlanillaEncabezado, fkClaveEmpleado);
+        }
+
+        public DataTable ObtenerEncabezado()
+        {
+            OdbcDataAdapter adapter = sn.ObtenerEncabezado();
+            DataTable tablaDeducciones = new DataTable();
+
+            if (adapter != null)
+            {
+                adapter.Fill(tablaDeducciones); // Llenar el DataTable con el resultado del OdbcDataAdapter
+            }
+            return tablaDeducciones;
+        }
+
+
+        public DataTable ObtenerDetalle()
+        {
+            OdbcDataAdapter adapter = sn.ObtenerDetalle();
+            DataTable tablaDeducciones = new DataTable();
+
+            if (adapter != null)
+            {
+                adapter.Fill(tablaDeducciones); // Llenar el DataTable con el resultado del OdbcDataAdapter
+            }
+            return tablaDeducciones;
+        }
+
+        public DataTable enviar(string tabla, string campo1, string campo2)
+        {
+
+            var dt1 = sn.obtener2(tabla, campo1, campo2);
+
+            return dt1;
+        }
+
+        public DataTable enviar2(string tabla, string campo1, string campo2)
+        {
+
+
+            /**/
+            var dt1 = sn.obtener2(tabla, campo1, campo2);
+
+            return dt1;
+        }
+
+        public bool AgregarPlanillaEncabezado(int correlativo, DateTime fechaInicio, DateTime fechaFinal, decimal totalMes)
+        {
+            Sentencias sentencias = new Sentencias();
+            return sentencias.InsertarPlanillaEncabezado(correlativo, fechaInicio, fechaFinal, totalMes);
+        }
+
+
+
+
+    }
+
+}
