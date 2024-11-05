cd35408e165b4ec70a4658a5ddcbb9b1a8f4b95a
jdcac <jcacaos1@miumg.edu.gt>
2024-11-04 21:36:37 -0600

    MDI

--- Codigo/Modulos/Contabilidad/Capa_Vista_Contabilidad/Contabilidad_MDI.cs
+++ Codigo/Modulos/Contabilidad/Capa_Vista_Contabilidad/Contabilidad_MDI.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using Capa_Vista_EstadosFinancieros;
+using Capa_Vista_EstadosFinancieros;
 //using Capa_Vista_CierreContable;
 using Capa_Vista_Presupuesto;
 //using Capa_Vista_Polizas;
